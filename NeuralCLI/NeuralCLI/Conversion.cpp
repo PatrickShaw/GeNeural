519a3aa1a5c618d6094e0a8116989ca6ff155d62
Patrick Shaw <psha67@student.monash.edu>
2017-08-19 18:39:28 +1000

    Updated neural

--- NeuralCLI/NeuralCLI/Conversion.cpp
+++ NeuralCLI/NeuralCLI/Conversion.cpp
@@ -6,7 +6,7 @@ namespace NeuralCLI {
 		std::shared_ptr<std::vector<T>> stdVector = std::make_shared<std::vector<T>>(cliArray->Length);
 		{
 			pin_ptr<T> pin(&cliArray[0]);
-			T *first(pin),	 *last(pin + cliArray->Length);
+			T *first(pin), *last(pin + cliArray->Length);
 			std::copy(first, last, stdVector->begin());
 		}
 		return stdVector;
@@ -14,7 +14,7 @@ namespace NeuralCLI {
 
 	template <typename T> array<T>^ Conversion::vector_to_array(const std::vector<T>& vector) {
 		array<T>^ cliArray = gcnew array<T> ^ (vector.size());
-		for (int i = 0; i < vector.size(); i++) {
+		for (size_t i = 0; i < vector.size(); i++) {
 			cliArray[i] = vector.at(i);
 		}
 		return cliArray;
